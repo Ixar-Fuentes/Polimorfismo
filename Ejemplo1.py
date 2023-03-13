e18700deb4114fa99968e485ba6a278641a54925
Ixar-Fuentes <127630850+Ixar-Fuentes@users.noreply.github.com>
2023-03-13 09:52:06 -0600

    Create Ejemplo1.py

---
+++ Ejemplo1.py
@@ -0,0 +1,14 @@
+class Persona1:
+    pass
+    def idioma():
+        return "Luis habla en Aleman"
+    
+class Persona2:
+    pass
+    def idioma():
+        return "Luis habla en Ruso"
+    
+persona1 =  Persona1
+persona2 =  Persona2
+print(persona1.idioma())
+print(persona2.idioma())
